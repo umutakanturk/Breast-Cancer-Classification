5557c5c21ed3a371df967b3e125b064f14b54cab
umutakanturk <umutaknturk@gmail.com>
2021-05-17 20:05:25 +0300

    PCA,NCA

--- cancer.py
+++ cancer.py
@@ -184,21 +184,87 @@ def KNN_Best_Params(x_train, x_test, y_train, y_test):
 
 grid = KNN_Best_Params(X_train, X_test, Y_train, Y_test)
 
+# %% PCA
+
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(x)
+
+pca = PCA(n_components=2)
+pca.fit(x_scaled)
+X_reduced_pca = pca.transform(x_scaled)
+pca_data = pd.DataFrame(X_reduced_pca, columns = ["p1", "p2"])
+pca_data["target"] = y
+sns.scatterplot(x="p1",y="p2",hue="target",data = pca_data)
+plt.title("PCA: p1 vs p2")
+
+X_train_pca, X_test_pca, Y_train_pca, Y_test_pca = train_test_split(X_reduced_pca,y,test_size = test_size,random_state=42)
+grid_pca = KNN_Best_Params(X_train_pca, X_test_pca, Y_train_pca, Y_test_pca)
+
+#Visualize
+cmap_light = ListedColormap(['Orange','cornflowerblue'])
+cmap_bold = ListedColormap(['darkorange','darkblue'])
+
+h = .05
+X = X_reduced_pca
+x_min, x_max = X[:,0].min() - 1, X[:,0].max() + 1
+y_min, y_max = X[:,0].min() - 1, X[:,0].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min,x_max,h),
+                     np.arange(y_min,y_max,h))
+
+Z = grid_pca.predict(np.c_[xx.ravel(), yy.ravel()])
+
+Z = Z.reshape(xx.shape)
+plt.figure()
+plt.pcolormesh(xx,yy,Z,cmap = cmap_light)
+
+plt.scatter(X[:,0], X[:,1],c=y,cmap=cmap_bold,
+            edgecolor='k',s=20)
+
+plt.xlim(xx.min(),xx.max())
+plt.ylim(yy.min(),yy.max())
+plt.title("%i-Class classification (k = %i,weights = '%s')"
+          % (len(np.unique(y)),grid_pca.best_estimator_.n_neighbors,grid_pca.best_estimator_.weights))
 
 
+# %% NCA
+nca = NeighborhoodComponentsAnalysis(n_components = 2, random_state=42)
+nca.fit(x_scaled, y)
+X_reduced_nca = nca.transform(x_scaled)
+nca_data = pd.DataFrame(X_reduced_nca, columns = ["p1","p2"])
+nca_data["target"] = y
+sns.scatterplot(x="p1",y="p2",hue ="target",data = nca_data)
+plt.title("NCA: p1 vs p2")
 
+X_train_nca, X_test_nca, Y_train_nca, Y_test_nca = train_test_split(X_reduced_nca,y,test_size = test_size,random_state=42)
 
+grid_nca = KNN_Best_Params(X_train_nca, X_test_nca, Y_train_nca, Y_test_nca)
 
+#Visualize
+cmap_light = ListedColormap(['Orange','cornflowerblue'])
+cmap_bold = ListedColormap(['darkorange','darkblue'])
 
+h = .2
+X = X_reduced_nca
+x_min, x_max = X[:,0].min() - 1, X[:,0].max() + 1
+y_min, y_max = X[:,0].min() - 1, X[:,0].max() + 1
 
+xx, yy = np.meshgrid(np.arange(x_min,x_max,h),
+                     np.arange(y_min,y_max,h))
 
+Z = grid_nca.predict(np.c_[xx.ravel(), yy.ravel()])
 
+Z = Z.reshape(xx.shape)
+plt.figure()
+plt.pcolormesh(xx,yy,Z,cmap = cmap_light)
 
+plt.scatter(X[:,0], X[:,1],c=y,cmap=cmap_bold,
+            edgecolor='k',s=20)
 
-
-
-
-
+plt.xlim(xx.min(),xx.max())
+plt.ylim(yy.min(),yy.max())
+plt.title("%i-Class classification (k = %i,weights = '%s')"
+          % (len(np.unique(y)),grid_nca.best_estimator_.n_neighbors,grid_nca.best_estimator_.weights))
 
 
 
